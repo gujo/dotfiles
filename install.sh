54971cd9f16579c3b8887dfb99357407f4bda65c
Johan Gunnarsson <Johan.Gunnarsson@UNIBET.com>
2013-09-27 13:28:40 +0200

    symlinks instead of files for easier update

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FILES="bash_profile bash_prompt bashrc tmux.conf"
+
+# back up old config files and replace with symlinks to dotfiles dir
+mkdir -p ~/.dotfiles_backup
+
+for file in $FILES; do
+    mv ~/.$file ~/.dotfiles_backup/
+    ln -s ~/dotfiles/$file ~/.$file
+done
